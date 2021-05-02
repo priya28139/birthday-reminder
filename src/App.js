827599607d07a50808173445dad37dcd33009659
Priya Aswani <priya.aswani28139@gmail.com>
2021-05-03 00:03:01 +0400

    Create and set people state variable in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 import List from "./components/List";
 export default function App() {
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List />
+        <h3>{data.length} birthdays today</h3>
+        <List people={people} />
         <button onClick={null}>Clear All</button>
       </section>
     </main>
